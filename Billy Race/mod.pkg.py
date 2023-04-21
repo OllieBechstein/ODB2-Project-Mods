cbf02fe17de5a27d09012356312d415a9193ada5
OllieBechstein <58312347+OllieBechstein@users.noreply.github.com>
2023-04-22 00:31:28 +0100

    Started working on new race, added template for new planet

---
+++ Billy Race/mod.pkg.py
@@ -0,0 +1,23 @@
+name = "Billy Race"
+version = "1.0"
+description = "Adds a new race to the game called Billy."
+contents = [
+    # a spreadsheet that defines new objects added to the game by your mod
+    # you probably need this one
+    
+    "things.xls",
+
+    # code for the new additions
+    
+    "code.py",
+    
+    # localized text for your mod
+    
+    "text-english.xls" / lang("english"), # this is the english version
+
+    # custom graphics/sound for your mod
+    # uncomment the lines below only after creating an asset bundle with the Slipways asset tool
+    # refer to the modding documentation for details on how to do that
+    
+    "assets.win", "assets.mac",
+]
